d1c8b11af04af0619eb21a267d20c7638ecae26e
Sergio Andaloro <andalorosergio@gmail.com>
2017-01-13 20:36:17 -0800

    Fixing hands glitch when they perform complete rotation.

--- 02 - JS + CSS Clock/index.ts
+++ 02 - JS + CSS Clock/index.ts
@@ -11,15 +11,29 @@ window.onload = () => {
 
         // convert the seconds 0 to 60 to a degree for the clock hand
         const secondsDegrees : number = ((seconds / 60) * 360) + 90; // add 90 since we shift the hands of 90 degrees via css
+        
+        // Before rotating let's see if we have completed an entire rotation.
+        resetTransition(secondHand, secondsDegrees);
         secondHand.style.transform = `rotate(${secondsDegrees}deg)`
 
         const mins : number = now.getMinutes();
         const minsDegrees : number = ((mins / 60) * 360) + 90;
+        resetTransition(minHand, minsDegrees);
         minHand.style.transform = `rotate(${minsDegrees}deg)`
 
         const hours : number = now.getHours();
         const hoursDegrees : number = ((hours / 12) * 360) + 90;
+        resetTransition(hourHand, hoursDegrees);
         hourHand.style.transform = `rotate(${hoursDegrees}deg)`
     }
     setInterval(setDate, 1000);
+}
+
+// Checks if the hand has complete an entire rotation, if so removes the transition to avoid glitch
+function resetTransition(element: HTMLElement, handDegrees: number) : void {
+    if(handDegrees === 90) {
+        element.classList.add('no-transition');
+    } else if (element.classList.contains('no-transition')) {
+        element.classList.remove('no-transition');
+    }
 }
